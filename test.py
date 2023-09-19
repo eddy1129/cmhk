9e29ba462158c2067309f44189544bf76ccc3641
Yuen Ho <98795653+eddy1129@users.noreply.github.com>
2023-09-19 10:26:41 +0800

    wre

--- test.py
+++ test.py
@@ -4,45 +4,39 @@ import matplotlib.pyplot as plt
 import urllib.request
 
 # Download the image
-image_url = "https://raw.githubusercontent.com/eddy1129/cmhk/master/test3.jpg"
-urllib.request.urlretrieve(image_url, "test3.jpg")
+image_url = "https://raw.githubusercontent.com/eddy1129/cmhk/master/test1.jpg"
+urllib.request.urlretrieve(image_url, "test1.jpg")
 
 # Load the image
-image = cv2.imread('test3.jpg')
+image = cv2.imread('test1.jpg')
 
-# Define the region of interest (ROI) for the right side of the image
-roi_x = image.shape[1] - 500  # Start from the last 500 pixels of the image
-roi_width = 500  # Width of the ROI
+# Convert the image to HSV
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-# Crop the ROI from the image
-roi = image[:, roi_x:roi_x + roi_width]
+# Define the color range for detecting the blue line
+lower_blue = np.array([100, 50, 50])
+upper_blue = np.array([140, 255, 255])
 
-# Define color thresholds for blue
-lower_blue = np.array([100, 0, 0], dtype=np.uint8)
-upper_blue = np.array([255, 100, 100], dtype=np.uint8)
+# Create a mask with the blue color range
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-# Create masks to identify blue pixels
-blue_pixels = cv2.inRange(roi, lower_blue, upper_blue)
+# Find the coordinates of the blue pixels in the last 500 pixels of the image
+blue_coords = np.column_stack(np.where(mask[:, -500:]))
 
-# Find the coordinates of blue pixels
-blue_coords = np.column_stack(np.where(blue_pixels))
+if blue_coords.size > 0:
+    # Find the minimum and maximum y-coordinates of the blue pixels
+    y_coords = blue_coords[:, 1]
+    y_min = np.min(y_coords)
+    y_max = np.max(y_coords)
 
-# Find the region where there is a significant drop in the blue line
-drop_region = []
-prev_y = None
-for bx, by in blue_coords:
-    if prev_y is not None and bx > prev_y + 5:  # Adjust the threshold as needed
-        drop_region.append((bx, by))
-    prev_y = bx
+    # Find the x-coordinate where the blue line starts to drop
+    x_coords = blue_coords[:, 0]
+    x_drop_start = np.min(x_coords[y_coords == y_max])
 
-# If we found a region where there is a significant drop in the blue line
-if drop_region:
-    drop_region = np.array(drop_region)
-    x_min, x_max = np.min(drop_region[:, 0]), np.max(drop_region[:, 0])
-    y_min, y_max = np.min(drop_region[:, 1]), np.max(drop_region[:, 1])
-
-    # Draw a rectangle on the original image to highlight the area
-    cv2.rectangle(image, (roi_x + y_min, x_min), (roi_x + y_max, x_max), (0, 0, 0), 2)
+    # Draw a rectangle to highlight the drop region
+    cv2.rectangle(image, (image.shape[1] - 500, x_drop_start), (image.shape[1], y_max), (0, 0, 0), 2)
+else:
+    print("No blue pixels detected")
 
 # Display the original image with the highlighted area
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
