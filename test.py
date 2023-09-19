4fa8629d0f5243ebda094ab9fc9f14d373829f9b
Yuen Ho <98795653+eddy1129@users.noreply.github.com>
2023-09-19 10:09:14 +0800

    wer

--- test.py
+++ test.py
@@ -17,39 +17,35 @@ roi_width = 500  # Width of the ROI
 # Crop the ROI from the image
 roi = image[:, roi_x:roi_x + roi_width]
 
-# Define color thresholds for blue and orange
+# Define color thresholds for blue
 lower_blue = np.array([100, 0, 0], dtype=np.uint8)
 upper_blue = np.array([255, 100, 100], dtype=np.uint8)
 
-lower_orange = np.array([0, 50, 150], dtype=np.uint8)
-upper_orange = np.array([100, 150, 255], dtype=np.uint8)
-
-# Create masks to identify blue and orange pixels
+# Create masks to identify blue pixels
 blue_pixels = cv2.inRange(roi, lower_blue, upper_blue)
-orange_pixels = cv2.inRange(roi, lower_orange, upper_orange)
 
-# Find the coordinates of blue and orange pixels
+# Find the coordinates of blue pixels
 blue_coords = np.column_stack(np.where(blue_pixels))
-orange_coords = np.column_stack(np.where(orange_pixels))
 
-# Find the region where blue is below orange
-below_region = []
+# Find the region where there is a significant drop in the blue line
+drop_region = []
+prev_y = None
 for bx, by in blue_coords:
-    for ox, oy in orange_coords:
-        if bx > ox and by == oy:
-            below_region.append((bx, by))
+    if prev_y is not None and bx > prev_y + 5:  # Adjust the threshold as needed
+        drop_region.append((bx, by))
+    prev_y = bx
 
-# If we found a region where blue is below orange
-if below_region:
-    below_region = np.array(below_region)
-    x_min, x_max = np.min(below_region[:, 0]), np.max(below_region[:, 0])
-    y_min, y_max = np.min(below_region[:, 1]), np.max(below_region[:, 1])
+# If we found a region where there is a significant drop in the blue line
+if drop_region:
+    drop_region = np.array(drop_region)
+    x_min, x_max = np.min(drop_region[:, 0]), np.max(drop_region[:, 0])
+    y_min, y_max = np.min(drop_region[:, 1]), np.max(drop_region[:, 1])
 
     # Draw a rectangle on the original image to highlight the area
-    cv2.rectangle(image, (roi_x + y_min, x_min), (roi_x + y_max, x_max), (0, 0, 255), 2)
+    cv2.rectangle(image, (roi_x + y_min, x_min), (roi_x + y_max, x_max), (0, 0, 0), 2)
 
 # Display the original image with the highlighted area
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-plt.title('Highlight Area When Blue Line is Below Orange Line')
+plt.title('Highlight Area of Blue Line Drop')
 plt.axis('off')
 plt.show()
